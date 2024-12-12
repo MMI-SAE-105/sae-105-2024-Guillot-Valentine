b7900564faf554f9b6a918abd0867c294cc77f90
Guillot-Valentine <valentine.guillot@edu.univ-fcomte.fr>
2024-12-12 01:57:40 +0100

    Debut HTML InfosPratique & Carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,32 @@
-// On cible les éléments à modifier
+// MENU
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
 const page = document.body;
 
-// Vérifier si les éléments existent avant d'ajouter l'événement
 if (toggle && nav) {
   toggle.addEventListener("click", () => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
-    // Mise à jour des attributs ARIA pour accessibilité
+
     toggle.ariaExpanded = isClosed;
     nav.ariaHidden = isOpen;
     page.classList.toggle("noscroll", isClosed);
+  });
+}
+
+
+//CAROUSEL
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__btn--prev");
+const nextButton = document.querySelector(".carousel__btn--next");
+
+if (carousel) {
+
+  prevButton.addEventListener("click", (evt) => {
+    carousel.scrollBy({ left: -300, behavior: "smooth" });
+  });
+
+  nextButton.addEventListener("click", (evt) => {
+    carousel.scrollBy({ left: +300, behavior: "smooth" });
   });
 }
